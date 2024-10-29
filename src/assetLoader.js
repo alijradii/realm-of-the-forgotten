6986cd3a2b906797731141c39b1a0a0c6008c1aa
amjadsh92 <amjadsharafeddien@gmail.com>
2024-10-30 00:54:53 +0200

    Loading the animation

---
+++ src/assetLoader.js
@@ -0,0 +1,92 @@
+const fighterAnimTemplate = {
+    idle: {
+        repeat: -1,
+        frameRate: 4,
+        up: {
+            frames: [80, 81, 82, 83],
+        },
+        down: {
+            frames: [40, 41, 42, 43],
+        },
+        left: {
+            frames: [0, 1, 2, 3],
+        },
+        right: {
+            frames: [0, 1, 2, 3],
+        },
+    },
+    walk: {
+        repeat: -1,
+        frameRate: 6,
+        up: {
+            frames: [88, 89, 90, 91],
+        },
+        down: {
+            frames: [48, 49, 50, 51],
+        },
+        left: {
+            frames: [8, 9, 10, 11],
+        },
+        right: {
+            frames: [8, 9, 10, 11],
+        },
+    },
+    attack: {
+        frameRate: 6,
+        up: {
+            frames: [96, 97, 98, 99],
+        },
+        down: {
+            frames: [56, 57, 58, 59],
+        },
+        left: {
+            frames: [16, 17, 18, 19],
+        },
+        right: {
+            frames: [16, 17, 18, 19],
+        },
+    }
+};
+
+function loadAssets(scene) {
+    classes.forEach((baseClass) => {
+        const colors = ["Red", "Blue", "Green"];
+
+        colors.forEach((color) => {
+            const name = `${baseClass}_${color}`;
+            const path = `assets/images/${baseClass}/${name}.png`;
+
+            scene.load.spritesheet(name, path, {
+                frameWidth: 32,
+                frameHeight: 32,
+            });
+        });
+    });
+}
+
+function loadAnimations(scene) {
+    classes.forEach((baseClass) => {
+        const colors = ["Red", "Blue", "Green"];
+
+        colors.forEach((color) => {
+            const name = `${baseClass}_${color}`;
+            const path = `assets/images/${baseClass}/${name}.png`;
+
+            Object.keys(fighterAnimTemplate).forEach((animation) => {
+                const mp = fighterAnimTemplate[animation];
+                const dirs = ["up", "down", "left", "right"];
+
+                dirs.forEach((dir) => {
+                    scene.anims.create({
+                        key: `${name}_${animation}_${dir}`,
+                        frameRate: mp.frameRate || 4,
+                        frames: scene.anims.generateFrameNames(name, {
+                            frames: mp[dir].frames,
+                        }),
+                        repeat: mp.repeat || 0,
+                    });
+                });
+            });
+        });
+    });
+}
